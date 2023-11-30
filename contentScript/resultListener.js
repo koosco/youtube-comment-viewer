17828d10e33cb421288e3dba819b389f786c65dd
koosco <rnxogud123@naver.com>
2023-11-30 21:26:46 +0900

    url message Listener가 request의 속성을 읽도록 변경
    moreContent 추가에 따라 기존에 있던 url message Listener가
    message에 url이 있을 때만 Listen하도록 변경

--- contentScript/resultListener.js
+++ contentScript/resultListener.js
@@ -1,8 +1,10 @@
 // 3. background에서 데이터 받음
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     // 5. DOM에 data 표시
-    var newItem = document.getElementById("newContentDiv-result");
-    newItem.innerHTML = request.result.text;
+    if(request.url){
+        var newItem = document.getElementById("newContentDiv-result");
+        newItem.innerHTML = request.result.text;
+    }
 });
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
