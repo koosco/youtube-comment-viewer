6bdeb4b651b487dbecfd8735c57c60b9d9b5408c
koosco <rnxogud123@naver.com>
2023-12-08 14:11:01 +0900

    DOM에 graph 추가
    * d3 library 추가
    * json을 받아와서 plot하는 기능 추가 필요

--- contentScript/newElement.js
+++ contentScript/newElement.js
@@ -6,9 +6,79 @@ setTimeout(() => {
     newHead.textContent = "Custom Div Tag";
     newDiv.appendChild(newHead);
 
-    var newContentDiv = document.createElement("div");
-    newContentDiv.setAttribute("id", "newContentDiv-result");
-    newDiv.appendChild(newContentDiv);
+    // var newContentDiv = document.createElement("div");
+    // newContentDiv.setAttribute("id", "newContentDiv-result");
+    // newDiv.appendChild(newContentDiv);
+    
+    // start
+    const width = 600;
+    const height = 400;
+
+    // 새로운 div 생성
+    var d3Container = document.createElement("div");
+    d3Container.setAttribute("id", "newContentDiv-result");
+    newDiv.appendChild(d3Container);
+
+    const svg = d3.select(d3Container)
+        .append("svg")
+        .attr("width", width)
+        .attr("height", height);
+
+    const jsonData = {
+        "nodes": [
+            { "id": "Word1", "size": 20 },
+            { "id": "Word2", "size": 30 },
+            { "id": "Word3", "size": 15 },
+            { "id": "Word4", "size": 40}
+            // 추가 노드들...
+        ],
+        "links": [
+            { "source": "Word1", "target": "Word2" },
+            { "source": "Word2", "target": "Word3" },
+            { "source": "Word2", "target": "Word4"}
+            // 추가 연결들...
+        ]
+    };
+
+    const simulation = d3.forceSimulation(jsonData.nodes)
+        .force("link", d3.forceLink(jsonData.links).id(d => d.id))
+        .force("charge", d3.forceManyBody().strength(-50))
+        .force("center", d3.forceCenter(width / 2, height / 2))
+        .force("collide", d3.forceCollide().radius(d => d.size + 15));
+
+    const link = svg.append("g")
+        .selectAll("line")
+        .data(jsonData.links)
+        .enter()
+        .append("line")
+        .style("stroke", "#999")
+        .style("stroke-width", 1);
+
+    const node = svg.append("g")
+        .selectAll("text")
+        .data(jsonData.nodes)
+        .enter()
+        .append("text")
+        .attr("x", d => d.x)
+        .attr("y", d => d.y)
+        .attr("dy", "0.3em")
+        .style("text-anchor", "middle")
+        .style("font-weight", "bold")
+        .style("font-size", d => d.size + "px")
+        .text(d => d.id);
+
+    simulation.on("tick", () => {
+        link
+            .attr("x1", d => d.source.x)
+            .attr("y1", d => d.source.y)
+            .attr("x2", d => d.target.x)
+            .attr("y2", d => d.target.y);
+
+        node
+            .attr("x", d => d.x)
+            .attr("y", d => d.y);
+    });
+    // end
 
     var moreButton = document.createElement('button');
     moreButton.textContent = "더 보기";
